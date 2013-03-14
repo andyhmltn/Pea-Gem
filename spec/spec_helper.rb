e055c1572d606d7d2b4699841b9cdc7fab2ffae0
Andy Hamilton <andyhmltn@gmail.com>
2013-03-14 10:50:23 +0000

    Added better formatting to the RSpec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 require_relative '../lib/pea'
 require 'yaml'
+
+RSpec.configure do |config|
+	# Enable colour
+	config.color_enabled = true
+	config.tty = true
+
+	config.formatter = :documentation
+end
